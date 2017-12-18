6d0fb453174a70d95d2146afba39d4b5639c8bce
torirepe <t.motoe@amd.tokyo>
2017-12-18 15:57:52 +0900

    追記

--- dest/assets/js/script.js
+++ dest/assets/js/script.js
@@ -1,14 +1,56 @@
 $(window).load(function(){
-  is();
+  hs();
+  hm()
   fa();
   cc();
+  ss();
+  ps()
 });
 
-function is(){
-  $("#heroSlider ul").bxSlider({
-    auto: true,
-    mode: "fade"
-  })
+function hs(){
+  
+  //変数の宣言
+  var ww,
+      config,
+      heroSlider,
+      timer = false;
+  
+  //オプションの設定
+  function cs() {
+    ww = $(window).innerWidth();
+    if(ww > 767) {
+      //pcのオプション
+      config = {
+        auto: true,
+        mode: "fade",
+        controls: false,
+        pager: false,
+      }
+    } else {
+      //spのオプション
+      config = {
+        auto: true,
+        controls: true,
+        pager: false,
+      }
+    }
+  };
+  
+  //初回読み込み
+  cs();
+  heroSlider = $("#heroSlider ul").bxSlider(config);
+  
+  //sldierのresize処理
+  $(window).resize(function() {
+    if (timer !== false) {
+      clearTimeout(timer);
+    }
+    timer = setTimeout(function() {
+      console.log('resized');
+      cs();
+      slider.reloadSlider(config);
+    }, 200);
+  });
 };
 
 
@@ -16,13 +58,24 @@ function fa(){
   $('[data-container]').fullpage({
     sectionSelector: '[data-section]',
     easingcss3: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)',
-    scrollingSpeed: 600,
+    scrollingSpeed: 480,
     scrollOverflow: 'true',
-    paddingTop: '70px',
     anchors: ['intro', 'BASKET_baller', 'SOCCER_baller', 'VOLLEY_baller', 'BEACH_baller'],
     //afterResize: function () {},
     //onLeave: function (index, nextIndex, direction) {},
     //afterLoad: function (anchorLink, index) {},
+  });
+};
+
+function hm(){
+  var header = $('#header');
+  var menu = $('#header .menu');
+  var nav = $('#header .menu nav li a');
+  menu.on('click', function(){
+    header.toggleClass('menuopen');
+  });
+  nav.on('click', function(){
+    header.toggleClass('menuopen');
   });
 };
 
@@ -31,13 +84,36 @@ var ccs;
 function cc(){
   $('[data-cct]').on('click', function(){
     ccs = $(this).closest('[data-ccs]');
-    $(this).removeClass('active');
-    
-    $(this).addClass('active');
     if( ccs.attr('data-ccs') == '1' ) {
       ccs.attr('data-ccs', '2');
     } else {
       ccs.attr('data-ccs', '1');
+    }
+  });
+}
+
+function ss(){
+  $('a[href^="#"]').click(function(){
+    var speed = 240;
+    var hh = $('#header').innerHeight();
+    var ww = $(window).innerWidth();
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    if(ww > 769) {
+      position = position - hh;
+    }
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+  });
+};
+
+function ps(){
+  $('[data-ps]').on('inview', function(event, isInView, visiblePartX, visiblePartY) {
+    if (isInView) {
+      $(this).attr('data-ps','1');
+    } else {
+      $(this).attr('data-ps','0');
     }
   });
 }
---
+++ source/index.php
@@ -0,0 +1,217 @@
+<!DOCTYPE html>
+<html lang="jp">
+  <head prefix="og: http://ogp.me/ns#">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+
+    <!--  favicons -->
+    <meta name="keywords" content="#">
+    <meta name="description" content="#">
+
+    <title>FUKUNARY feat. MIKASA</title>
+
+    <!--  favicons -->
+    <link rel="shortcut icon" type="image/vnd.microsoft.icon" href="/favicons/favicon.ico">
+    <link rel="icon" type="image/vnd.microsoft.icon" href="/favicons/favicon.ico">
+    <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="/favicons/android-chrome-192x192.png">
+    <link rel="icon" type="image/png" sizes="48x48" href="/favicons/favicon-48x48.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-160x160.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicons/favicon-196x196.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png">
+    <link rel="manifest" href="/favicons/manifest.json">
+    <meta name="msapplication-TileColor" content="#2d88ef">
+    <meta name="msapplication-TileImage" content="/mstile-144x144.png">
+    <!-- /favicons -->
+
+    <!-- ogp -->
+    <meta property="og:title" content="FUKUNARY feat. MIKASA">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="#">
+    <meta property="og:url" content="https://fukunaryfeatmikasa.com/">
+    <meta property="og:image" content="https://fukunaryfeatmikasa.com/og.png">
+    <meta property="og:site_name" content="FUKUNARY feat. MIKASA">
+    <!-- /ogp -->
+
+    <!-- google font -->
+    <link href="https://fonts.googleapis.com/css?family=Prata" rel="stylesheet">
+    <!-- /google font -->
+   
+    <!-- assets -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/jquery.fullpage.css">
+    <script src="assets/js/picturefill.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.inview.min.js"></script>
+    <script src="assets/js/bxslider.min.js"></script>
+    <script src="assets/js/scrolloverflow.min.js"></script>
+    <script src="assets/js/jquery.easings.min.js"></script>
+    <script src="assets/js/jquery.fullPage.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+    <!-- /assets -->
+
+  </head>
+  <body>
+    <header id="header">
+      <h1 class="pc"><a href="#top"><img src="assets/images/logo_bk.svg" alt="FUKUNARY feat.Mikasa"></a></h1>
+      <div class="menu">
+        <div class="pc">menu</div>
+        <div class="st">
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+          <div>&nbsp;</div>
+        </div>
+        <nav>
+          <ul class="ganv">
+            <li><a href="#top">top</a></li>
+            <li><a href="#news">news</a></li>
+            <li><a href="#concept">concept</a></li>
+            <li><a href="#basket">BASKET baller</a></li>
+            <li><a href="#soccer">SOCCER baller</a></li>
+            <li><a href="#volley">VOLLEY baller</a></li>
+            <li><a href="#beach">BEACH baller</a></li>
+          </ul>
+          <ul class="st sns">
+            <li><a target="_blank" href="https://www.instagram.com/fukunary/"><img src="assets/images/icon_instagram_wh.svg" alt="instagram"></a></li>
+            <li><a target="_blank" href="https://twitter.com/fukunary/"><img src="assets/images/icon_twitter_wh.svg" alt="twitter"></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/fukunary/"><img src="assets/images/icon_facebook_wh.svg" alt="facebook"></a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="pc sns">
+        <div>share</div>
+        <ul>
+          <li><a target="_blank" href="https://www.instagram.com/fukunary/"><img src="assets/images/icon_instagram_bk.svg" alt=""></a></li>
+          <li><a target="_blank" href="https://twitter.com/fukunary/"><img src="assets/images/icon_twitter_bk.svg" alt=""></a></li>
+          <li><a target="_blank" href="https://www.facebook.com/fukunary/"><img src="assets/images/icon_facebook_bk.svg" alt=""></a></li>
+        </ul>
+      </div>
+    </header>
+    <main id="main">
+      <section id="top" class="hero">
+        <h1><img src="assets/images/logo_wh.svg" alt="FUKUNARY feat.Mikasa"></h1>
+        <div id="heroSlider">
+          <ul>
+            <li class="c_01"><img src="assets/images/hero_bg_01-tp.jpg" alt="イメージ"></li>
+            <li class="c_02"><img src="assets/images/hero_bg_02-tp.jpg" alt="イメージ"></li>
+            <li class="c_03"><img src="assets/images/hero_bg_03-tp.jpg" alt="イメージ"></li>
+            <li class="c_04"><img src="assets/images/hero_bg_04-tp.jpg" alt="イメージ"></li>
+            <li class="c_05"><img src="assets/images/hero_bg_05-tp.jpg" alt="イメージ"></li>
+            <li class="c_06"><img src="assets/images/hero_bg_06-tp.jpg" alt="イメージ"></li>
+          </ul>
+        </div>
+      </section>
+      <section id="news" class="news">
+        <div class="inner">
+          <h2>news</h2>
+          <?php echo file_get_contents('https://yahashisouin.com/include-news/'); ?>
+          <div><a target="_blank" href="">過去の記事はこちら（八橋装院コーポレートサイトへ）</a></div>
+        </div>
+      </section>
+      <section id="concept" class="concept">
+        <div class="inner">
+          <h2>concept</h2>
+          <h3>広島縫製でボール生地が変身</h3>
+          <p>まったく異なる業種の２社がコラボレーション。<br class="tp">2社をつなぐ「HIROSHIMAのものづくり」という接点から実現しました。</p>
+          <p>国際公式球として認定を受ける「MIKASA」製のボールと、<br class="tp">日本の多くのアパレルブランドを長年支え続けてきた八橋装院の縫製技術。<br class="tp">2つが融合し生まれたブランド・シリーズが「FUKUNARY feat. MIKASA」です。</p>
+          <p>ボール素材が持つ耐久性と軽さが魅力のバッグを中心とした、<br class="tp">「毎日使える」ファッションアイテムを展開しています。<br class="tp">「シティボーイが持つスポーツバッグ」をテーマに、シンプルなデザインを提案します。</p>
+        </div>
+      </section>
+      <section data-ps="0" id="basket" class="products">
+        <div class="l_inner">
+          <h3 class="right">
+            <picture>
+              <source media="(max-width:425px)" srcset="assets/images/products_ttl_01-sp.png">
+              <source media="(min-width:426px)" srcset="assets/images/products_ttl_01-tp.png">
+              <img src="assets/images/products_ttl_01-sp.png" alt="">
+            </picture>
+            BASKET baller
+          </h3>
+          <div class="btn"><a target="_blank" href="">view detail</a></div>
+        </div>
+        <div class="scroll"><a href="#soccer">scroll</a></div>
+        <div class="pic">
+          <img src="assets/images/products_pic_01-tp.jpg" alt="商品画像">
+        </div>
+      </section>
+      <section data-ps="0" data-ccs="1" id="soccer" class="products">
+        <div class="l_inner">
+          <h3 class="left">
+            <picture>
+              <source media="(max-width:425px)" srcset="assets/images/products_ttl_02-sp.png">
+              <source media="(min-width:426px)" srcset="assets/images/products_ttl_02-tp.png">
+              <img src="assets/images/products_ttl_02-sp.png" alt="">
+            </picture>
+            SOCCER baller
+          </h3>
+          <div class="btn"><a target="_blank" href="">view detail</a></div>
+        </div>
+        <div data-cct class="color"><img src="assets/images/icon_change.svg" alt= "アイコン">color</div>
+        <div class="scroll"><a href="#volley">scroll</a></div>
+        <div data-ccp="1" class="pic">
+          <img src="assets/images/products_pic_02-tp.jpg" alt="商品画像">
+        </div>
+        <div data-ccp="2" class="pic">
+          <img src="assets/images/products_pic_03-tp.jpg" alt="商品画像">
+        </div>
+      </section>
+      <section data-ps="0" data-ccs="1" id="volley" class="products">
+        <div class="l_inner">
+          <h3 class="right">
+            <picture>
+              <source media="(max-width:425px)" srcset="assets/images/products_ttl_03-sp.png">
+              <source media="(min-width:426px)" srcset="assets/images/products_ttl_03-tp.png">
+              <img src="assets/images/products_ttl_03-sp.png" alt="">
+            </picture>
+            VOLLEY baller
+          </h3>
+          <div class="btn"><a target="_blank" href="#beach">view detail</a></div>
+        </div>
+        <div data-cct class="color"><img src="assets/images/icon_change.svg" alt= "アイコン">color</div>
+        <div class="scroll"><a href="#beach">scroll</a></div>
+        <div data-ccp="1" class="pic">
+          <img src="assets/images/products_pic_04-tp.jpg" alt="商品画像">
+        </div>
+        <div data-ccp="2" class="pic">
+          <img src="assets/images/products_pic_05-tp.jpg" alt="商品画像">
+        </div>
+      </section>
+      <section data-ps="0" id="beach" class="products">
+        <div class="l_inner">
+          <h3 class="left">
+            <picture>
+              <source media="(max-width:425px)" srcset="assets/images/products_ttl_04-sp.png">
+              <source media="(min-width:426px)" srcset="assets/images/products_ttl_04-tp.png">
+              <img src="assets/images/products_ttl_034-sp.png" alt="">
+            </picture>
+            BEACH baller
+          </h3>
+          <div class="btn"><a target="_blank" href="">view detail</a></div>
+        </div>
+        <div class="pic">
+          <img src="assets/images/products_pic_06-tp.jpg" alt="商品画像">
+        </div>
+      </section>
+    </main>
+    <footer>
+      <div>
+        <div class="yahashisouin"><a target="_blank" href="https://yahashisouin.com/"><img src="assets/images/footer_logo_01.svg" alt="yahashisouin"></a></div>
+        <div class="fukunary"><a target="_blank" href="https://fukunary.co.jp/"><img src="assets/images/footer_logo_02.svg" alt="fukunary"></a></div>
+        <div class="mikasa"><a target="_blank" href="http://www.mikasasports.co.jp/"><img src="assets/images/footer_logo_03.svg" alt="mikasa"></a></div>
+        <div class="pagetop"><a href="#top"></a></div>
+      </div>
+      <p><small>Copyright 2017 FUKUNARY All Rights Reserved.</small></p>
+    </footer>
+  </body>
+</html>
